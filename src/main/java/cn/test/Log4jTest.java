d58418ae110fa9ec020806f725657205586de778
liusheng <52142826@qq.com>
2020-07-08 14:06:41 +0800

    System out

--- src/main/java/cn/test/Log4jTest.java
+++ src/main/java/cn/test/Log4jTest.java
@@ -6,6 +6,6 @@ public class Log4jTest {
     public static void main(String[] args) {
     Logger logger = LogManager.getLogger(Log4jTest.class.getName());
     logger.info("Hello world");
-    System.out.println("Hello world");
+//    System.out.println("Hello world");
     }
 }
